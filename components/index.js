4d90bd23e6ad526d8e943a91a7bc40c21be202e3
Tristan Guest <tristanguest@gmail.com>
2022-02-23 23:16:46 -0400

    Build mdx-capable blog page

---
+++ components/index.js
@@ -0,0 +1,7 @@
+export { default as DarkMode } from "./DarkMode/DarkMode";
+export { default as Footer } from "./Footer/Footer";
+export { default as Social } from "./Social/Social";
+export { default as Layout } from "./Layout/Layout";
+export { default as BlogLayout } from "./Layout/BlogLayout";
+export { default as Header } from "./Header/Header";
+export { default as BlogList } from "./BlogList/BlogList";
