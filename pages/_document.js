d389fd7b18ec910756c963bdb885b98315af72b0
Tristan Guest <tristanguest@gmail.com>
2022-04-19 19:14:33 -0300

    Add font caching and preloading

--- pages/_document.js
+++ pages/_document.js
@@ -4,20 +4,20 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        {/* <link
+        <link
           rel="preload"
-          href="/fonts/Inter-roman.var.woff2"
+          href="/fonts/inter-v8-latin-regular.woff2"
           as="font"
           type="font/woff2"
           crossOrigin="anonymous"
         />
         <link
           rel="preload"
-          href="/fonts/Inter-italic.var.woff2"
+          href="/fonts/inter-v8-latin-900.woff2"
           as="font"
           type="font/woff2"
           crossOrigin="anonymous"
-        /> */}
+        />
       </Head>
       <body>
         <Main />
