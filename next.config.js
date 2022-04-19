d9f85119ff4afdbd639a7f01243305b0b563d72f
Tristan Guest <tristanguest@gmail.com>
2022-04-19 18:50:06 -0300

    Re-add fonts using google-webfonts-helper

--- next.config.js
+++ next.config.js
@@ -2,27 +2,27 @@ module.exports = {
   reactStrictMode: true,
 };
 
-module.exports = {
-  async headers() {
-    return [
-      {
-        source: "/fonts/Inter-roman.var.woff2",
-        headers: [
-          {
-            key: "Cache-control",
-            value: "public, immutable, max-age=31536000",
-          },
-        ],
-      },
-      {
-        source: "/fonts/Inter-italic.var.woff2",
-        headers: [
-          {
-            key: "Cache-control",
-            value: "public, immutable, max-age=31536000",
-          },
-        ],
-      },
-    ];
-  },
-};
+// module.exports = {
+//   async headers() {
+//     return [
+//       {
+//         source: "/fonts/Inter-roman.var.woff2",
+//         headers: [
+//           {
+//             key: "Cache-control",
+//             value: "public, immutable, max-age=31536000",
+//           },
+//         ],
+//       },
+//       {
+//         source: "/fonts/Inter-italic.var.woff2",
+//         headers: [
+//           {
+//             key: "Cache-control",
+//             value: "public, immutable, max-age=31536000",
+//           },
+//         ],
+//       },
+//     ];
+//   },
+// };
