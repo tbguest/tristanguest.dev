fc8c81842e204650994ed7b1ec3ded5fd739e738
Tristan Guest <tristanguest@gmail.com>
2022-05-09 18:11:26 -0300

    Add profile image sizes

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -5,8 +5,15 @@ import profile from "../../public/profile.png";
 const Header = () => {
   return (
     <header className={styles.header}>
-      <div className={styles.photo_container}>
-        <Image src={profile} alt="Profile photo" layout="fill" />
+      <div className={styles.profile_image_container}>
+        <Image
+          src={profile}
+          alt="Photo of the author"
+          width={180}
+          height={180}
+          priority={true}
+          className={styles.profile_image}
+        />
       </div>
       <h1 className={styles.title}>Tristan Guest</h1>
     </header>
