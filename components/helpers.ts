cdc4a310df86d26108c719ddb0b18545d0f0eae6
Tristan Guest <tristanguest@gmail.com>
2022-07-21 08:55:31 -0300

    Fix type errors

--- components/helpers.ts
+++ components/helpers.ts
@@ -1,11 +1,10 @@
-export const dateToString = (props) => {
-  const date = new Date(props);
-  const options = {
+export const dateToString = (dstring: Date) => {
+  const date = new Date(dstring);
+  const datestring = date.toLocaleDateString("en-US", {
     year: "numeric",
     month: "long",
     day: "numeric",
     timeZone: "UTC",
-  };
-  const datestring = date.toLocaleDateString("en-US", options);
+  });
   return datestring;
 };
