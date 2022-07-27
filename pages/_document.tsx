f5e278d2ec78abd0b7e39479f8517dcb20e7c509
Tristan Guest <tristanguest@gmail.com>
2022-07-27 09:31:31 -0300

    Add meta tags (#10)

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,7 @@
 import { Html, Head, Main, NextScript } from "next/document";
 // eslint-disable-next-line @next/next/no-script-in-document
 import Script from "next/script";
+import { config } from "../meta.config";
 
 export default function Document() {
   return (
@@ -19,6 +20,44 @@ export default function Document() {
           as="font"
           type="font/woff2"
           crossOrigin="anonymous"
+        />
+
+        <meta name="application-name" content={config.appName} />
+        <meta name="theme-color" content={config.themeColor} />
+
+        {/* <meta name="twitter:card" content="summary_large_image" key="twcard" /> */}
+        <meta name="twitter:card" content="summary" key="twcard" />
+        <meta name="twitter:url" content={config.domain} />
+        <meta name="twitter:title" content={config.appShortName} />
+        <meta name="twitter:description" content={config.description} />
+        <meta
+          name="twitter:image"
+          content={`${config.domain}/assets/lunaOceans.png`}
+        />
+        <meta
+          name="twitter:creator"
+          content={config.twitterHandle}
+          key="twhandle"
+        />
+        <meta name="twitter:site" content={config.twitterHandle} />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content={config.domain} key="ogtitle" />
+        <meta
+          property="og:description"
+          content={config.description}
+          key="ogdesc"
+        />
+        <meta
+          property="og:site_name"
+          content={config.appName}
+          key="ogsitename"
+        />
+        <meta property="og:url" content={config.domain} key="ogurl" />
+        <meta
+          property="og:image"
+          content={`${config.domain}/assets/tristan.png`}
+          key="ogimage"
         />
       </Head>
       <body>
