755df3aae5b6be476eb91b79c340b51acdc4a0bf
Tristan Guest <tristanguest@gmail.com>
2023-09-15 09:30:47 -0300

    Some type organization

--- components/BlogList/BlogList.tsx
+++ components/BlogList/BlogList.tsx
@@ -1,16 +1,15 @@
 import Link from "next/link";
-import { BlogPosts } from "../../models";
 import { dateToString } from "../helpers";
 import styles from "./BlogList.module.css";
+import { BlogPosts } from "../../pages/blog";
 
 const BlogList = ({ posts }: BlogPosts) => {
   return (
     <ul className={styles.container}>
       {posts?.map((post, index) => (
         <li key={index} className={styles.blog_item}>
-          <Link href={"/blog/" + post.slug}>
-            <a className={styles.blog_link}>
-              {/* <div className={styles.image_container}>
+          <Link href={"/blog/" + post.slug} className={styles.blog_link}>
+            {/* <div className={styles.image_container}>
                 <Image
                   src={post.frontMatter.thumbnailUrl}
                   alt={post.frontMatter.thumbnailAlt}
@@ -18,23 +17,22 @@ const BlogList = ({ posts }: BlogPosts) => {
                   className={styles.image}
                 />
               </div> */}
-              <div className={styles.blog_link_content}>
-                <>
-                  <h2>{post.frontMatter.title}</h2>
-                  <p className={styles.small_text}>
-                    <small>{dateToString(post.frontMatter.published)}</small>
-                  </p>
-                </>
-                <p>{post.frontMatter.description}</p>
-                <span className={styles.keywords_wrapper}>
-                  {post.frontMatter.tags.map((tag) => (
-                    <span className={styles.keywords} key={String(tag)}>
-                      <small>{tag}</small>
-                    </span>
-                  ))}
-                </span>
-              </div>
-            </a>
+            <div className={styles.blog_link_content}>
+              <>
+                <h2>{post.frontMatter.title}</h2>
+                <p className={styles.small_text}>
+                  <small>{dateToString(post.frontMatter.published)}</small>
+                </p>
+              </>
+              <p>{post.frontMatter.description}</p>
+              <span className={styles.keywords_wrapper}>
+                {post.frontMatter.tags.map((tag) => (
+                  <span className={styles.keywords} key={String(tag)}>
+                    <small>{tag}</small>
+                  </span>
+                ))}
+              </span>
+            </div>
           </Link>
         </li>
       ))}
