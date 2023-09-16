d8266e944425ac8981f51b9c9723ab52a61b4976
Tristan Guest <tristanguest@gmail.com>
2023-09-16 14:08:10 -0300

    Refactor document head

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Navbar, Footer } from "..";
 import styles from "./Layout.module.css";
+import Head from "next/head";
 
 type Props = {
   children: React.ReactNode;
@@ -9,6 +10,16 @@ type Props = {
 const Layout: React.FC<Props> = ({ children }) => {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Tristan Guest</title>
+        <meta property="og:title" content={`Tristan Guest`} key="title" />
+        <meta
+          name="description"
+          content="I'm a software developer working on the Atlantic coast of Canada. I value simplicity and usability in software, and I like building with full-stack tools. I think modern JavaScript is up there with the bicycle as a benchmark of human ingenuity."
+          key="desc"
+        />
+        <link rel="icon" href="/favicon.png" />
+      </Head>
       <Navbar />
       <main>{children}</main>
       <Footer />
