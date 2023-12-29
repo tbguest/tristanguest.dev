203c57796e4bb5ec725eef1d3819f90c2cfcb145
tbguest <tristanguest@gmail.com>
2023-12-29 16:58:14 -0400

    Remove blog page

--- components/index.ts
+++ components/index.ts
@@ -2,8 +2,6 @@ export { default as DarkMode } from "./DarkMode/DarkMode";
 export { default as Footer } from "./Footer/Footer";
 export { default as Social } from "./Social/Social";
 export { default as Layout } from "./Layout/Layout";
-export { default as BlogLayout } from "./Layout/BlogLayout";
 export { default as Header } from "./Header/Header";
-export { default as BlogList } from "./BlogList/BlogList";
 export { default as Navbar } from "./Navbar/Navbar";
 export { default as Home } from "./Home/Home";
