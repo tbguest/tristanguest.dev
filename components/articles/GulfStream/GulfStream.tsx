318b35616e79110c17bf542fe16e239e1ae23021
tbguest <tristanguest@gmail.com>
2025-01-03 16:36:36 -0400

    Fix some ts

--- components/articles/GulfStream/GulfStream.tsx
+++ components/articles/GulfStream/GulfStream.tsx
@@ -1,30 +1,19 @@
-import { useRouter } from "next/router";
 import { useRef, useState } from "react";
 import { ContentLayout } from "../../../components/layout/ContentLayout/ContentLayout";
 import { useParticleAnimation } from "../../../hooks/content/use-particle-animation";
 
-// TODO
-export const meta = () => {
-  return [
-    { title: "Instability I/O" },
-    {
-      name: "Instability I/O",
-      content: "Exploring simple physical models with JavaScript",
-    },
-  ];
-};
+export type GyreModel = "gulfStream" | "stommel" | "stommelBeta" | "munk";
 
 export default function GulfStream() {
-  const [model, setModel] = useState("gulfStream");
+  const [model, setModel] = useState<GyreModel>("gulfStream");
   const screenCanvasRef = useRef<HTMLCanvasElement | null>(null);
 
-  useParticleAnimation(model, screenCanvasRef.current);
+  useParticleAnimation(model as GyreModel, screenCanvasRef.current);
 
   return (
     <ContentLayout
       title="Why is there a Gulf Stream?"
       subtitle="Fun with geophysical fluid dynamics (and your GPU)"
-      path="/content/gulf-stream"
     >
       <article className="flex flex-col gap-8">
         <div className="flex relative items-center justify-center px-20 py-5">
